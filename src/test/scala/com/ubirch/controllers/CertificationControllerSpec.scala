0bbf9fcc460909e0dc69bcc56ab058edee01bf5f
CarlosAugusto <caaugustoss@gmail.com>
2021-07-27 12:03:30 +0200

    UNG-2957 Adding tests for wrong params

--- src/test/scala/com/ubirch/controllers/CertificationControllerSpec.scala
+++ src/test/scala/com/ubirch/controllers/CertificationControllerSpec.scala
@@ -1,6 +1,6 @@
 package com.ubirch.controllers
 
-import com.ubirch.{ Awaits, ExecutionContextsTests, InjectorHelperImpl }
+import com.ubirch.{ Awaits, ExecutionContextsTests, InjectorHelperImpl, Service }
 import io.prometheus.client.CollectorRegistry
 import org.scalatest.BeforeAndAfterEach
 import org.scalatra.test.scalatest.ScalatraWordSpec
@@ -20,10 +20,20 @@ class CertificationControllerSpec
       succeed
     }
     "fail when no auth-token is provided" in {
-      succeed
+      val expectedBody = s"""{"version":"${Service.version}","ok":false,"data":"Sorry, there is something invalid in your request: Header is missing (X-Auth-Token)"}"""
+      val device = "8e6a5679-5b0c-4a1e-9aa5-4f6e42fff5b3"
+      post("/" + device) {
+        assert(status == 400)
+        assert(body == expectedBody)
+      }
     }
     "fail when the device id is not a uuid" in {
-      succeed
+      val expectedBody = s"""{"version":"${Service.version}","ok":false,"data":"Sorry, there is something invalid in your request: Expected deviceId in UUID format"}""".stripMargin
+      val device = "this_is_not_a_uuid"
+      post("/" + device, Array.empty[Byte], Map("X-Auth-Token" -> "this is a token")) {
+        assert(status == 400)
+        assert(body == expectedBody)
+      }
     }
     "response headers include dcc id header and request id header" in {
       succeed
